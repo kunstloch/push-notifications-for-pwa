05954dc79cd40c97549c5d6579945e76254db18c
Alexander Scherer <alexscherer@outlook.com>
2020-05-05 20:31:00 +0200

    Add bootstrap and some content

--- components/nav.js
+++ components/nav.js
@@ -11,7 +11,7 @@ const links = [
 }))
 
 const Nav = () => (
-  <nav>
+  <>
     <Head>
       <meta charset='utf-8' />
       <meta http-equiv='X-UA-Compatible' content='IE=edge' />
@@ -20,13 +20,30 @@ const Nav = () => (
       <meta name='keywords' content='Keywords' />
       <title>Next.js PWA Example</title>
 
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />      
       <link rel='manifest' href='/manifest.json' />
       <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
       <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
       <link rel='apple-touch-icon' href='/apple-icon.png'></link>
       <meta name='theme-color' content='#317EFB' />
     </Head>
-    <ul>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <a class="navbar-brand" href="">nasenbär</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"> <a class="nav-link" href="">Contact</a></li>
+                <li class="nav-item"> <a class="nav-link" href="">Pricing</a></li>
+                <li class="nav-item"> <a class="nav-link" href="">Download</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    {/* --- Comes from: npx create-next-app --- */}
+    {/* <ul>
       <li>
         <Link href='/'>
           <a>Home</a>
@@ -37,9 +54,9 @@ const Nav = () => (
           <a href={href}>{label}</a>
         </li>
       ))}
-    </ul>
+    </ul> */}
 
-    <style jsx>{`
+    {/* <style jsx>{`
       :global(body) {
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
@@ -63,8 +80,8 @@ const Nav = () => (
         text-decoration: none;
         font-size: 13px;
       }
-    `}</style>
-  </nav>
+    `}</style> */}
+  </>
 )
 
 export default Nav
