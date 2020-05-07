b0021e07d44a49fc066907310cbcb2945f7fa6bb
Alexander Scherer <alexscherer@outlook.com>
2020-05-07 16:28:56 +0200

    Change styling of title

--- components/nav.js
+++ components/nav.js
@@ -21,24 +21,30 @@ const Nav = () => (
       <meta name='keywords' content='Keywords' />
       <title>Next.js PWA Example</title>
 
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&family=Ubuntu:wght@400;700&display=swap" rel="stylesheet" />
+      {/* --- Googel Font --- */}
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&family=Ubuntu:wght@400;700&display=swap" rel="stylesheet" />
 
+      {/* --- Font Awesome --- */}
+      <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+      {/* --- Bootstrap CSS --- */}
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
-      {/* <link rel="stylesheet" href="css/styles.css" /> */}
+      
+      {/* --- PWA --- */}
       <link rel='manifest' href='/manifest.json' />
       <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
       <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
       <link rel='apple-touch-icon' href='/apple-icon.png'></link>
       <meta name='theme-color' content='#317EFB' />
       
-      {/* --- Bootstrap --- */}
+      {/* --- Bootstrap Scripts --- */}
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     </Head>
     
-    <nav class="navbar navbar-expand-lg navbar-light font-weight-bold">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="">fakeApp</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- public/sw.js
+++ public/sw.js
@@ -130,10 +130,10 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "3eefcd56d3f5bfcc8b7c33d935f42689"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "dd8b7ed4a6cc2a91c5d701b425ed0012"
+    "revision": "058ca29150b048751343978cee20ed78"
   }, {
     "url": "/_next/static/runtime/webpack.js.map",
-    "revision": "7a2d979885844f144428ae955c9b406d"
+    "revision": "b1e9bbbf8d0ac2ab2aa5c462c2a7f330"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
